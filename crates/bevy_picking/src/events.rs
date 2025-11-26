fc82356cd28a1418e4cbdd1e0c50f3fbbcaab5fa
BT <dad@baozaolaoba.top>
2025-11-26 17:22:49 +0800

    fix(拖拽drag): 尝试修复拖拽panic

--- crates/bevy_picking/src/events.rs
+++ crates/bevy_picking/src/events.rs
@@ -766,7 +766,10 @@ pub fn pointer_events(
                             .flat_map(|h| h.iter().map(|(entity, data)| (*entity, data.to_owned())))
                             .filter(|(hovered_entity, _)| *hovered_entity != *drag_target)
                         {
-                            *state.dragging_over.get_mut(&hovered_entity).unwrap() = hit.clone();
+                            // *state.dragging_over.get_mut(&hovered_entity).unwrap() = hit.clone();
+                            if let Some(value) = state.dragging_over.get_mut(&hovered_entity) {
+                                *value = hit.clone();
+                            }
                             let drag_over_event = Pointer::new(
                                 pointer_id,
                                 location.clone(),
